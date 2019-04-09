3fe1f18e0ac4a87a6258612069a5e45340f30567
angeloorru <angeloorru18@gmail.com>
2019-04-09 11:09:15 +0100

    etting up servlet base project to write a react app (??).

---
+++ src/main/java/FormServlet.java
@@ -0,0 +1,43 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "FormServlet", urlPatterns = "/calculateServlet")
+public class FormServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String height = request.getParameter("height");
+        String weight = request.getParameter("weight");
+
+        try {
+            double bmi = calculateBMI(
+                    Double.parseDouble(weight),
+                    Double.parseDouble(height));
+
+            request.setAttribute("bmi", bmi);
+            response.setHeader("Test", "Success");
+            response.setHeader("BMI", String.valueOf(bmi));
+
+            RequestDispatcher dispatcher
+                    = request.getRequestDispatcher("index.html");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            response.sendRedirect("index.html");
+        }
+    }
+
+    private Double calculateBMI(Double weight, Double height) {
+        return weight / (height * height);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
