7fb3cd2a829069f94cf8dc1d69ae286203e19421
angeloorru <angeloorru18@gmail.com>
2019-04-23 11:12:55 +0100

    Code tidying up.

--- web/app/components/home/homeCtrl.js
+++ web/app/components/home/homeCtrl.js
@@ -1,7 +1,3 @@
 angular.module('appHomeModule', []).controller('homeCtrl', ['$scope', 'homeContent', function ($scope, homeContent) {
-    /*
-    homeContent.getContent().then(function(response){
-        $scope.dataset = response;
-    });*/
     $scope.dataset = homeContent.getContent();
 }]);
